b05d11be01a6a47bea23f6e8a297cdaa38b95246
BASHA <BASHA@LAPTOP-R2PN0OJJ>
2021-08-11 05:47:56 +0530

    second commit- adding Swap.java file to second repsository

---
+++ secondrepository/src/secondrepository/Swap.java
@@ -0,0 +1,17 @@
+package secondrepository;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+int t;
+t=a;
+a=b;
+b=t;
+System.out.println(a);
+System.out.println(b);
+	}
+
+}
